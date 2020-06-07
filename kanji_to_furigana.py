f52a6c0ae1db440620998c16d95953cb3013ce3a
jacob <shnaidmanj@gmail.com>
2020-06-07 03:24:50 -0400

    fixed to work with katakana readings

--- kanji_to_furigana.py
+++ kanji_to_furigana.py
@@ -35,7 +35,7 @@ class KanjiToFurigana():
         time.sleep(0.15)
         self.controller.release(key)
 
-    def getFurigana(self, kanji: str):
+    def getFurigana_Kanji(self, kanji: str):
         url = f'https://jisho.org/search/{kanji}'
         try:
             page = requests.get(url)
@@ -45,7 +45,50 @@ class KanjiToFurigana():
             furigana_block = answer_block.find(class_='furigana')
             furigana_spans = furigana_block.find_all("span")
             furigana = "".join([span.getText() for span in furigana_spans])
+            if (furigana):
+                self.kanji_dict[kanji] = furigana
             return furigana
+        except Exception:
+            return None
+
+    def getFurigana(self, sentence: str):
+        url = f'https://jisho.org/search/{sentence}'
+        try:
+            page = requests.get(url)
+            content = page.content.decode('utf-8', 'ignore')
+            soup = BeautifulSoup(content, 'html.parser')
+            word_ul = soup.find("section", id="zen_bar").find(
+                "ul", class_="clearfix")
+            word_list = [li for li in word_ul.contents if li.name == 'li']
+            furigana_sentence = ""
+            for word_li in word_list:
+                furigana = word_li.find("span",
+                                        class_="japanese_word__furigana_wrapper").text
+                furigana = furigana.replace("\n","").replace(" ","")
+                word_orig = word_li.find("span",
+                                        class_="japanese_word__text_wrapper").text
+                word_orig = word_orig.replace("\n", "").replace(" ", "")
+                if(furigana):
+                    orig_iter = iter(reversed(word_orig))
+                    furi_iter = iter(reversed(furigana))
+                    try:
+                        orig_char = next(orig_iter)
+                        furi_char = next(furi_iter)
+                        num_common = 0
+                        while (orig_char == furi_char):
+                            num_common += 1
+                            orig_char = next(orig_iter)
+                            furi_char = next(furi_iter)
+                    except StopIteration:
+                        pass
+                    if (num_common != 0):
+                        furigana = furigana[:-num_common]
+                        word_orig = word_orig[:-num_common]
+
+                    furigana_sentence += " {}[{}]".format(word_orig, furigana)
+                else:
+                    furigana_sentence += word_orig
+            return furigana_sentence
         except Exception:
             return None
 
@@ -61,18 +104,11 @@ class KanjiToFurigana():
         time.sleep(0.1)
         text_clipboard = clipboard.paste()
 
-        kanji_list = self.kanji_block_reg.findall(text_clipboard)
-        if (not kanji_list):
-            return
-        for kanji in kanji_list:
-            if kanji not in self.kanji_dict:
-                furigana = self.getFurigana(kanji)
-                if (furigana):
-                    self.kanji_dict[kanji] = furigana
-        self.furigana_clipboard = self.kanji_block_reg.sub(
-            self.match_to_anki_furigana, text_clipboard)
+        if self.kanji_block_reg.search(text_clipboard):
+            self.furigana_clipboard = self.getFurigana(text_clipboard)
 
     def on_paste(self):
+        print(self.furigana_clipboard)
         # Save actual clipboard
         _ = clipboard.paste()
         # Output our internal clipboard
@@ -96,7 +132,8 @@ arg_parser.add_argument('--paste_key',
                         'See https://pynput.readthedocs.io/en/latest/keyboard.html#pynput.keyboard.Key for more info.',
                         default='<ctrl>+<cmd>+v'
                         )
-arg_parser.add_argument('-e', action='store_true', default=False, dest='exit_key', help='Allows program to quit by pressing <ctrl>+d')
+arg_parser.add_argument('-e', action='store_true', default=False,
+                        dest='exit_key', help='Allows program to quit by pressing <ctrl>+d')
 args = arg_parser.parse_args()
 
 kanji_to_furigana = KanjiToFurigana()
