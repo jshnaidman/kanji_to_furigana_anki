8b2f02a4fefa8219fc5760d75ef55d0b26c8d4a8
jacob <shnaidmanj@gmail.com>
2020-06-13 19:51:51 -0400

    Simplifies script to use nihongodera.com

--- kanji_to_furigana.py
+++ kanji_to_furigana.py
@@ -4,12 +4,13 @@ from pynput import keyboard
 from pynput.keyboard import Key, HotKey
 import re
 import clipboard
-from typing import List, Match, Dict
+from typing import List, Match, Dict, Type, Union
 import requests
 from bs4 import BeautifulSoup
 import time
 import argparse
 import traceback
+from threading import Thread, Lock
 
 
 class KanjiToFurigana():
@@ -19,14 +20,14 @@ class KanjiToFurigana():
         self.furigana_clipboard = ""
         self.controller = keyboard.Controller()
         self.paste_hotkeys = {}
+        self.lock = Lock()
         self.kanji_reg = re.compile(r'[㐀-䶵一-鿋豈-頻]+')
-        self.not_jap_reg = re.compile(
-            r'[^\u3040-\u30ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff\uff66-\uff9f]+')
-        self.jap_reg = re.compile(
-            r'[\u3040-\u30ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff\uff66-\uff9f]+')
+        self.japanese_reg = re.compile(
+            r'[\u3040-\u30ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff\uff66-\uff9f\(\)]+')
+        self.furigana_res_reg = re.compile(r'(.*)\((.*)\)')
         self.on_copy()
 
-    def set_hotkeys(self, hotkeys: Dict):
+    def set_hotkeys(self, hotkeys: Dict) -> None:
         for hotkey in hotkeys:
             try:
                 if hotkeys[hotkey].__name__ == 'on_paste':
@@ -35,112 +36,67 @@ class KanjiToFurigana():
             except Exception:
                 continue
 
-    def tap_key(self, key):
+    def tap_key(self, key: Union[str, Type[Key]]) -> None:
         self.controller.press(key)
         time.sleep(0.15)
         self.controller.release(key)
 
-    def get_furigana_kanji(self, kanji: str):
-        url = f'https://jisho.org/search/{kanji}'
+    def get_furigana(self, sentence: str) -> str:
         try:
-            page = requests.get(url)
-            content = page.content.decode('utf-8', 'ignore')
+            payload = {
+                'type': 'furigana',
+                'text': sentence
+            }
+            resp = requests.post('https://nihongodera.com/tools/convert', data=payload)
+            if (resp.status_code != 200):
+                raise Exception(f"Got HTTP code: {resp.status_code}")
+            content = resp.text
             soup = BeautifulSoup(content, 'html.parser')
-            answer_block = soup.find("div", class_="exact_block")
-            furigana_block = answer_block.find(class_='furigana')
-            furigana_spans = furigana_block.find_all("span")
-            furigana = "".join([span.getText() for span in furigana_spans])
-            if (furigana):
-                self.kanji_dict[kanji] = furigana
-            return furigana
+            answer_block = soup.find("div", class_="tool__results")
+            furigana_res = []
+            for child in answer_block.children:
+                if child.name == 'ruby':
+                    match = self.furigana_res_reg.search(child.getText())
+                    furigana = ' {}[{}]'.format(match.group(1), match.group(2))
+                    furigana_res.append(furigana)
+                else:
+                    japanese = self.japanese_reg.search(str(child))
+                    if (japanese):
+                        furigana_res.append(japanese.group(0))
+            return ''.join(furigana_res)
         except Exception:
             traceback.print_exc()
-            return None
+            return sentence
 
-    def _get_furigana_from_zenbar(self, zen_bar):
-        word_ul = zen_bar.find("ul", class_="clearfix")
-        word_list = [li for li in word_ul.contents if li.name == 'li']
-        furigana_sentence = ""
-        for word_li in word_list:
-            furigana_wrapper = word_li.find("span",class_="japanese_word__furigana_wrapper")
-            span_children = [child for child in furigana_wrapper.children if child.name == 'span']
-            if (span_children):
-                for span in span_children:
-                    if span.name != 'span':
-                        continue
-                    furigana = span.attrs.get('data-text')
-                    if (furigana and furigana != span.text):
-                        furigana_sentence += " {}[{}]".format(furigana, span.text)
-                    else:
-                        furigana_sentence += span.text
-            else:
-                furigana_sentence += word_li.find("span",class_="japanese_word__text_wrapper").text
-        return furigana_sentence
-
-    def _get_furigana_from_main_result(self, soup):
-        main_result = soup.find("div", id="main_results").find(
-            "div", class_="exact_block")
-        reading_div = main_result.find("div", class_="japanese", lang="ja")
-
-        furigana_span = reading_div.find(class_="furigana")
-        furigana_list = [self.jap_reg.search(content.text) if content.name == 'span'
-                         else self.jap_reg.search(content) for content in furigana_span.contents]
-        furigana_list = [part.group(0) for part in furigana_list if part]
-
-        text_span = reading_div.find("span", class_="text")
-        text = self.not_jap_reg.sub("", text_span.text)
-
-        furigana_idx = 0
-        furigana_sentence = ""
-        for char in text:
-            if (furigana_idx < len(furigana_list) and self.kanji_reg.search(char)):
-                furigana_sentence += " {}[{}]".format(
-                    char, furigana_list[furigana_idx])
-                furigana_idx += 1
-            else:
-                furigana_sentence += char
-
-        return furigana_sentence
-
-    def get_furigana(self, sentence: str):
-        url = f'https://jisho.org/search/{sentence}'
-        try:
-            page = requests.get(url)
-            content = page.content.decode('utf-8', 'ignore')
-            soup = BeautifulSoup(content, 'html.parser')
-            zen_bar = soup.find("section", id="zen_bar")
-            if (zen_bar):
-                return self._get_furigana_from_zenbar(zen_bar)
-            else:
-                return self._get_furigana_from_main_result(soup)
-        except Exception:
-            traceback.print_exc()
-            return None
-
-    def match_to_anki_furigana(self, match: Match):
-        kanji = match.group(0)
-        if kanji in self.kanji_dict:
-            return f" {kanji}[{self.kanji_dict[kanji]}]"
-        return kanji
-
-    def on_copy(self):
+    def _copy_thread(self):
         # I sleep just to make sure there's no race condition with ctrl+c setting the new clipboard
         # and me getting the clipboard here
         time.sleep(0.1)
         text_clipboard = clipboard.paste()
         # This just removes extra formatting for convenience
-        clipboard.copy(text_clipboard) 
+        clipboard.copy(text_clipboard)
         if self.kanji_reg.search(text_clipboard):
             self.furigana_clipboard = self.get_furigana(text_clipboard)
+        self.lock.release()
 
-    def on_paste(self):
+    def on_copy(self) -> None:
+        # We launch this in a different thread so that we don't
+        # block on I/O which can lock up the keyboard.
+        if(self.lock.acquire(blocking=False)):
+            t = Thread(target=self._copy_thread)
+            t.start()
+
+    def on_paste(self) -> None:
         # Save actual clipboard
         _ = clipboard.paste()
+        self.lock.acquire()
+        furi_clipboard = self.furigana_clipboard
+        self.lock.release()
         try:
             # Output our internal clipboard
-            clipboard.copy(self.furigana_clipboard)
+            clipboard.copy(furi_clipboard)
         except:
-            print(f"failed to paste: {self.furigana_clipboard}")
+            print(f"failed to paste: {furi_clipboard}")
             print(f"old clipboard: {_}")
             clipboard.copy(_)
             traceback.print_exc()
@@ -155,20 +111,25 @@ class KanjiToFurigana():
 
 
 arg_parser = argparse.ArgumentParser(
-    description='Adds Furigana to Kanji in clipboard.')
+    description='Adds Furigana to Kanji in clipboard. Press ctrl+cmd+alt+d to quit')
 arg_parser.add_argument('--paste_key',
                         dest='paste_key',
                         help='the key press to paste the furigana contents.'
-                        ' eg: ./kanji_to_furigana --paste_key <cmd>+<shift>+v - '
-                        'See https://pynput.readthedocs.io/en/latest/keyboard.html#pynput.keyboard.Key for more info.',
+                        ' eg: ./kanji_to_furigana --paste_key "<alt>+<ctrl>+<cmd>+v ',
                         default='<ctrl>+<cmd>+v'
+                        )
+arg_parser.add_argument('--copy_key',
+                        dest='copy_key',
+                        help='The key combination to copy. Mac users should enter <cmd>+c'
+                        ' eg: ./kanji_to_furigana --copy_key "<cmd>+c ',
+                        default='<ctrl>+c'
                         )
 args = arg_parser.parse_args()
 
 kanji_to_furigana = KanjiToFurigana()
 
 hotkeys = {
-    '<ctrl>+c': kanji_to_furigana.on_copy,
+    args.copy_key: kanji_to_furigana.on_copy,
     args.paste_key: kanji_to_furigana.on_paste,
     '<ctrl>+<cmd>+<alt>+d': sys.exit
 }
